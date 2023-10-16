5190dd9ee266b813a9ac904e6711dde5324c5b07
cevtuocjw <cjwcjwwww8560@163.com>
2023-10-16 14:51:01 +0800

    2

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/hanmilLee'
+      href='https://github.com/cevtuocjw'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
